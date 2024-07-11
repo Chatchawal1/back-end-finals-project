09cbe0b84dee77885ecdffaa89326cd88712a9c5
metharae <metharae@gosoft.co.th>
2024-07-11 12:42:18 +0700

    [metja]

--- api/Borrower.js
+++ api/Borrower.js
@@ -22,7 +22,7 @@ router.put("/return", (req, res) => {
         const updateLoanDetailsQuery = `
           UPDATE loan_details
           SET loan_status = 'คืน'
-          WHERE loan_id = ? AND equipment_name = ?;
+          WHERE loan_id = ?;
         `;
 
         db.query(
